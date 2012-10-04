afadd10a43d906530abf1a71ae9936a2cf208889
Oleg Rybak <oleg.rybak@gmail.com>
2012-10-04 21:07:29 +0400

    Added BuildStartContextProcessor stub

---
+++ server/src/jetbrains/buildServer/sharedResources/server/services/SharedResourcesContextProcessor.java
@@ -0,0 +1,20 @@
+package jetbrains.buildServer.sharedResources.server.services;
+
+import jetbrains.buildServer.serverSide.BuildStartContext;
+import jetbrains.buildServer.serverSide.BuildStartContextProcessor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 04.10.12
+ * Time: 21:06
+ *
+ * @author Oleg Rybak
+ */
+public class SharedResourcesContextProcessor implements BuildStartContextProcessor {
+
+  @Override
+  public void updateParameters(@NotNull BuildStartContext context) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
